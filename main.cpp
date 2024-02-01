af6ebf8ae95414003e86096208711a59386fea32
Smig <ahmedharoun370@gmail.com>
2024-02-01 15:56:07 -0500

    Asking for user inputs
    Implemented the first part of the lab, where we ask the user for inputs

--- main.cpp
+++ main.cpp
@@ -1,1 +1,26 @@
 // Group 2 LAB01 CS291 //
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+	cout << "Welcome to Lost Fortune" << endl << endl;
+
+	// variables to hold user inputs
+	int firstNumber = 0;
+	// second number should be smaller than first number
+	int secondNumber = 0;
+	string lastName = "";
+
+	cout << "Please enter the following for your personalized adventure" << endl;
+	cout << "Enter a number: ";
+	cin >> firstNumber;
+	cout << "Enter a number, smaller than the first: ";
+	cin >> secondNumber;
+	cout << "Enter your last name: ";
+	cin >> lastName;
+	
+}
