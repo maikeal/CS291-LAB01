f88a6ba6116cf68ae40d9540040b4cf9226c2561
Smig <ahmedharoun370@gmail.com>
2024-02-05 10:28:44 -0500

    Commit by Humayra
    Finished the last part of the Lab assignment

--- main.cpp
+++ main.cpp
@@ -1,26 +1,73 @@
-// Group 2 LAB01 CS291 //
+// Group 2 LAB01 CS291 // 
+ 
 
-#include <iostream>
-#include <string>
+#include <iostream> 
+#include <string> 
 
-using namespace std;
+using namespace std; 
 
-int main() {
+int main() { 
 
-	cout << "Welcome to Lost Fortune" << endl << endl;
+ 
 
-	// variables to hold user inputs
-	int firstNumber = 0;
-	// second number should be smaller than first number
-	int secondNumber = 0;
-	string lastName = "";
+	cout << "Welcome to Lost Fortune" << endl << endl; 
 
-	cout << "Please enter the following for your personalized adventure" << endl;
-	cout << "Enter a number: ";
-	cin >> firstNumber;
-	cout << "Enter a number, smaller than the first: ";
-	cin >> secondNumber;
-	cout << "Enter your last name: ";
-	cin >> lastName;
-	
-}
+ 
+
+	// variables to hold user inputs 
+
+	int firstNumber = 0; 
+
+	// second number should be smaller than first number 
+
+	int secondNumber = 0; 
+
+	string lastName = ""; 
+
+ 
+
+	cout << "Please enter the following for your personalized adventure" << endl; 
+
+	cout << "Enter a number: "; 
+
+	cin >> firstNumber; 
+
+	cout << "Enter a number, smaller than the first: "; 
+
+	cin >> secondNumber; 
+
+	cout << "Enter your last name: "; 
+
+	cin >> lastName; 
+
+ 
+
+	cout << "A brave group of " << firstNumber << " set out on a quest -- in search of the lost treasure of the Ancient Dwarves. "; 
+
+	cout << "The group was led by that legendary rogue, " << lastName << ".\n"; 
+
+ 
+
+    int remainingAdventurers = firstNumber - secondNumber; 
+
+    cout << "Along the way, a band of marauding ogres ambushed the party. All fought bravely under the command of " << lastName << ", "; 
+
+    cout << "and the ogres were defeated, but at a cost. Of the adventurers, " << secondNumber << " were vanquished, leaving just " << remainingAdventurers << " in the group.\n"; 
+
+ 
+
+    cout << "The party was about to give up all hope. But while laying the deceased to rest, they stumbled upon the buried fortune. "; 
+
+     
+
+    int goldPieces = 900;   
+
+ 
+
+    cout << "So the adventurers split " << goldPieces << " gold pieces. " << lastName << " held on to the extra 4 pieces to keep things fair, of course.\n"; 
+
+ 
+
+    return 0; 
+
+} 
